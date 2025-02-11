3b5f297f1b3b1b2e2ae6a2d1c76653501207721a
Leg-10n <kevin.mon@student.mahidol.edu>
2025-02-11 10:24:07 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+onload = () => {
+  setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+  }, 1000);
+  setTimeout(() => {
+      let textElement = document.querySelector(".text");
+      if (textElement) {
+          textElement.style.display = "block";
+          textElement.style.opacity = "1";
+          textElement.style.animation = "text-appear 2s forwards";
+      }
+  }, 12000);
+
+  setTimeout(() => {
+      let textElement = document.querySelector(".text");
+      if (textElement) {
+          textElement.style.animation = "text-disappear 2s forwards";
+      }
+  }, 26000);
+
+  setTimeout(() => {
+      let finalTextElement = document.querySelector(".final-text");
+      if (finalTextElement) {
+          finalTextElement.style.display = "block";
+          finalTextElement.style.animation = "final-text-appear 2s forwards";
+      }
+  }, 28000);
+
+  setTimeout(() => {
+    document.body.classList.remove("flowers-disappear");
+}, 10000);
+};
