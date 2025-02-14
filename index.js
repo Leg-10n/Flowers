c0c7742e023829102f3878c7b29100c8532cce7f
Leg-10n <kevin.mon@student.mahidol.edu>
2025-02-14 12:25:08 +0700

    fix: added more text

--- index.js
+++ index.js
@@ -19,6 +19,36 @@ onload = () => {
   }, 26000);
 
   setTimeout(() => {
+    let textElement = document.querySelector(".second-text");
+    if (textElement) {
+        textElement.style.display = "block";
+        textElement.style.animation = "text-appear 2s forwards";
+    }
+}, 12000);
+
+setTimeout(() => {
+    let textElement = document.querySelector(".second-text");
+    if (textElement) {
+        textElement.style.animation = "text-disappear 2s forwards";
+    }
+}, 26000);
+
+setTimeout(() => {
+    let textElement = document.querySelector(".third-text");
+    if (textElement) {
+        textElement.style.display = "block";
+        textElement.style.animation = "text-appear 2s forwards";
+    }
+}, 12000);
+
+setTimeout(() => {
+    let textElement = document.querySelector(".third-text");
+    if (textElement) {
+        textElement.style.animation = "text-disappear 2s forwards";
+    }
+}, 26000);
+
+  setTimeout(() => {
       let finalTextElement = document.querySelector(".final-text");
       if (finalTextElement) {
           finalTextElement.style.display = "block";
